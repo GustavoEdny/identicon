b2672190eb78671901a8efdfaf86bf806911ae71
Gustavo Edny <gustavoedny@gmail.com>
2020-04-08 18:33:41 -0300

    Adding buil_grid method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,25 @@ defmodule Identicon do
     input
     |> Identicon.hash_input
     |> Identicon.pick_color
+    |> Identicon.build_grid
+  end
+
+  def build_grid(image) do
+    grid =
+      Enum.chunk(image.hex, 3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [165, 22, 10]
+    [first, second | _tail] = row
+
+    # [165, 22, 10, 22, 165]
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
