5a12b098ab6157f550af936b81aece6472b591da
Gustavo Edny <gustavoedny@gmail.com>
2020-04-08 16:01:40 -0300

    Removing auto generated code

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,17 +2,4 @@ defmodule Identicon do
   @moduledoc """
     A tool that creates user profile icons like github profile icons
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
